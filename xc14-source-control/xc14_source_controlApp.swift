93ae85957e0e9576628c9129badbd4b7328d7f06
Dr. Z <dr.zufall@gmail.com>
2023-02-11 21:58:14 -0500

    Initial Commit

---
+++ xc14-source-control/xc14_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc14_source_controlApp.swift
+//  xc14-source-control
+//
+//  Created by Scott Zufall on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct xc14_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
